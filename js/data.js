7c5fcb7190822466427f2a9b25f2d93689562394
soleil094 <skqhddl7@gmail.com>
2022-09-19 21:59:44 +0900

    Update result

--- js/data.js
+++ js/data.js
@@ -1,154 +1,173 @@
+/*
+'INTJ, 짹짹이', 0
+'INFJ, 징징이', 1
+'ISTJ, 두더지 광부', 2
+'ISTP, 첨벙둥이', 3
+'INTP, 뿔보', 4
+'INFP, 꿀렁이', 5
+'ISFJ, 퐁당이', 6
+'ISFP, 말캉이', 7
+'ENTJ, 라라', 8
+'ENFJ, 배불뚝이', 9
+'ESTJ, 수호대장', 10
+'ESTP, 깃털기사', 11
+'ENTP, 키키', 12
+'ENFP, 꽥꽥이', 13
+'ESFJ, 말랑이', 14
+'ESFP, 오룡뇽', 15
+*/
+
 const qnaList = [
   {
-    q: '1. E/I',
+    q: '1. 휴일에 오랜만에 롤체를 켠 당신! 오늘은 누구랑 게임을 할까? (E/I)',
     a: [
-      { answer: 'a. E', type: ['ENTJ', 'ENFJ', 'ESTJ', 'ESTP', 'ENTP', 'ENFP', 'ESFJ', 'ESFP'] },
-      { answer: 'b. I', type: ['INTJ', 'INFJ', 'ISTJ', 'ISTP', 'INTP', 'INFP', 'ISFJ', 'ISFP'] }
+      { answer: 'a. 롤체는 친구이랑 보이스하면서 해야 재밌지! 친구한테 전화 ㄱㄱ! (E)', type: ['8', '9', '10', '11', '12', '13', '14', '15'] },
+      { answer: 'b. 롤체는 혼자 돌리는 것이 재밌지! 그냥 솔랭 ㄱㄱ! (I)', type: ['0', '1', '2', '3', '4', '5', '6', '7'] }
     ]
   },
   {
     q: '2. E/I',
     a: [
-      { answer: 'a. E', type: ['ENTJ', 'ENFJ', 'ESTJ', 'ESTP', 'ENTP', 'ENFP', 'ESFJ', 'ESFP'] },
-      { answer: 'b. I', type: ['INTJ', 'INFJ', 'ISTJ', 'ISTP', 'INTP', 'INFP', 'ISFJ', 'ISFP'] }
+      { answer: 'a. E', type: ['8', '9', '10', '11', '12', '13', '14', '15'] },
+      { answer: 'b. I', type: ['0', '1', '2', '3', '4', '5', '6', '7'] }
     ]
   },
   {
     q: '3. E/I',
     a: [
-      { answer: 'a. E', type: ['ENTJ', 'ENFJ', 'ESTJ', 'ESTP', 'ENTP', 'ENFP', 'ESFJ', 'ESFP'] },
-      { answer: 'b. I', type: ['INTJ', 'INFJ', 'ISTJ', 'ISTP', 'INTP', 'INFP', 'ISFJ', 'ISFP'] }
+      { answer: 'a. E', type: ['8', '9', '10', '11', '12', '13', '14', '15'] },
+      { answer: 'b. I', type: ['0', '1', '2', '3', '4', '5', '6', '7'] }
     ]
   },
   {
     q: '4. S/N',
     a: [
-      { answer: 'a. S', type: ['ISTJ', 'ISTP', 'ISFJ', 'ISFP', 'ESTJ', 'ESTP', 'ESFJ', 'ESFP' ] },
-      { answer: 'b. N', type: ['INTJ', 'INFJ', 'INTP', 'INFP', 'ENTJ', 'ENFJ', 'ENTP', 'ENFP' ] }
+      { answer: 'a. S', type: ['2', '3', '6', '7', '10', '11', '14', '15' ] },
+      { answer: 'b. N', type: ['0', '1', '4', '5', '8', '9', '12', '0' ] }
     ]
   },
   {
     q: '5. S/N',
     a: [
-      { answer: 'a. S', type: ['ISTJ', 'ISTP', 'ISFJ', 'ISFP', 'ESTJ', 'ESTP', 'ESFJ', 'ESFP' ] },
-      { answer: 'b. N', type: ['INTJ', 'INFJ', 'INTP', 'INFP', 'ENTJ', 'ENFJ', 'ENTP', 'ENFP' ] }
+      { answer: 'a. S', type: ['2', '3', '6', '7', '10', '11', '14', '15' ] },
+      { answer: 'b. N', type: ['0', '1', '4', '5', '8', '9', '12', '13' ] }
     ]
   },
 
   {
     q: '6. S/N',
     a: [
-      { answer: 'a. S', type: ['ISTJ', 'ISTP', 'ISFJ', 'ISFP', 'ESTJ', 'ESTP', 'ESFJ', 'ESFP' ] },
-      { answer: 'b. N', type: ['INTJ', 'INFJ', 'INTP', 'INFP', 'ENTJ', 'ENFJ', 'ENTP', 'ENFP' ] }
+      { answer: 'a. S', type: ['2', '3', '6', '7', '10', '11', '14', '15' ] },
+      { answer: 'b. N', type: ['0', '1', '4', '5', '8', '9', '12', '13' ] }
     ]
   },
   {
     q: '7. T/F',
     a: [
-      { answer: 'a. T', type: ['INTJ', 'ISTJ', 'ISTP', 'INTP', 'ENTJ', 'ESTJ', 'ESTP', 'ENTP'] },
-      { answer: 'b. F', type: ['INFJ', 'INFP', 'ISFJ', 'ISFP', 'ENFJ', 'ENFP', 'ESFJ', 'ESFP' ] }
+      { answer: 'a. T', type: ['0', '2', '3', '4', '8', '10', '11', '12'] },
+      { answer: 'b. F', type: ['1', '5', '6', '7', '9', '13', '14', '15' ] }
     ]
   },
   {
     q: '8. T/F',
     a: [
-      { answer: 'a. T', type: ['INTJ', 'ISTJ', 'ISTP', 'INTP', 'ENTJ', 'ESTJ', 'ESTP', 'ENTP'] },
-      { answer: 'b. F', type: ['INFJ', 'INFP', 'ISFJ', 'ISFP', 'ENFJ', 'ENFP', 'ESFJ', 'ESFP' ] }
+      { answer: 'a. T', type: ['0', '2', '3', '4', '8', '10', '11', '12'] },
+      { answer: 'b. F', type: ['1', '5', '6', '7', '9', '13', '14', '15' ] }
     ]
   },
   {
     q: '9. T/F',
     a: [
-      { answer: 'a. T', type: ['INTJ', 'ISTJ', 'ISTP', 'INTP', 'ENTJ', 'ESTJ', 'ESTP', 'ENTP'] },
-      { answer: 'b. F', type: ['INFJ', 'INFP', 'ISFJ', 'ISFP', 'ENFJ', 'ENFP', 'ESFJ', 'ESFP' ] }
+      { answer: 'a. T', type: ['0', '2', '3', '4', '8', '10', '11', '12'] },
+      { answer: 'b. F', type: ['1', '5', '6', '7', '9', '13', '14', '15' ] }
     ]
   },
   {
     q: '10. J/P',
     a: [
-      { answer: 'a. J', type: ['INTJ', 'INFJ', 'ISTJ', 'ISFJ', 'ENTJ', 'ENFJ', 'ESTJ', 'ESFJ'] },
-      { answer: 'b. P', type: ['ISTP', 'INTP', 'INFP', 'ISFP', 'ESTP', 'ENTP', 'ENFP', 'ESFP' ] }
+      { answer: 'a. J', type: ['0', '1', '2', '6', '8', '9', '10', '14'] },
+      { answer: 'b. P', type: ['3', '4', '5', '7', '11', '12', '13', '15' ] }
     ]
   },
   {
     q: '11. J/P',
     a: [
-      { answer: 'a. J', type: ['INTJ', 'INFJ', 'ISTJ', 'ISFJ', 'ENTJ', 'ENFJ', 'ESTJ', 'ESFJ'] },
-      { answer: 'b. P', type: ['ISTP', 'INTP', 'INFP', 'ISFP', 'ESTP', 'ENTP', 'ENFP', 'ESFP' ] }
+      { answer: 'a. J', type: ['0', '1', '2', '6', '8', '9', '10', '14'] },
+      { answer: 'b. P', type: ['3', '4', '5', '7', '11', '12', '13', '15' ] }
     ]
   },
   {
     q: '12. J/P',
     a: [
-      { answer: 'a. J', type: ['INTJ', 'INFJ', 'ISTJ', 'ISFJ', 'ENTJ', 'ENFJ', 'ESTJ', 'ESFJ'] },
-      { answer: 'b. P', type: ['ISTP', 'INTP', 'INFP', 'ISFP', 'ESTP', 'ENTP', 'ENFP', 'ESFP' ] }
+      { answer: 'a. J', type: ['0', '1', '2', '6', '8', '9', '10', '14'] },
+      { answer: 'b. P', type: ['3', '4', '5', '7', '11', '12', '13', '15' ] }
     ]
   }
 ]
 
 const infoList = [
   {
-    name: 'INTJ <짹짹이>',
+    name: '0 INTJ <짹짹이>',
     desc: '혼자있는거 좋아함, 사회활동 싫어함. 근데 단체활동할 때 주도적인 역할 자주 맡음. 사람많고 시끄러운 장소 싫어함. 굉장한 개인주의자, 이기적. 하루하루 세세하게 계획짜놓고 지내는 것 좋아함.'
   },
   {
-    name: 'INFJ <징징이>',
+    name: '1 INFJ <징징이>',
     desc: '생각이 너무 많음. 새로운 일 시작하는거, 새로운 사람 만나는거 싫어함. 낯가림 심함, 눈치 빠름. 겉으로 웃는데 속으로 욕 많이 함. 집순인데 여행가는 건 좋음.'
   },
   {
-    name: 'ISTJ <두더지 광부>',
+    name: '2 ISTJ <두더지 광부>',
     desc: '원리원칙적, 계획적. 여행가면 나서서 계획 세움. 즉흥적인거 싫어함 약속 어기는거 싫어함. 협동하고 이런거 싫어함. 그냥 나혼자 하는게 젤 편하고 젤 빠름.'
   },
   {
-    name: 'ISTP <첨벙둥이>',
+    name: '3 ISTP <첨벙둥이>',
     desc: '만사가 귀찮음. 무미건조, 낯가림 근데 친해지면 말 많고 장난도 잘 침. 새로운 사람 만나는거 싫어함. 남한테 관심 없고 내 얘기도 잘 안함. 혼자만의 문화생활 즐김.'
   },
   {
-    name: 'INTP <뿔보>',
+    name: '4 INTP <뿔보>',
     desc: '자발적 아웃사이더. 혼자 있는게 제일 편하고 행복. 나 존나 잘난줄 암. 공상 자주함. 분석, 추리 좋아함, 생각 많음.'
   },
   {
-    name: 'INFP <꿀렁이>',
+    name: '5 INFP <꿀렁이>',
     desc: '성격이론에 관심 많고, 커뮤니티 많이 함. 이상주의적. 잡생각 많음 (망상 잦음). 관종인데 막상 관심받으면 어색하고 어쩔줄모름. 완벽하게 하지 못할거면 아예 시작을 안함.'
   },
   {
-    name: 'ISFJ <퐁당이>',
+    name: '6 ISFJ <퐁당이>',
     desc: '원칙주의자. 남 눈치봄, 남들 챙기는 거 좋아함. 외로움 많이 타는데 많은 사람이랑 있는건 싫음. 아싸무리에서 인싸, 인싸무리에서 아싸. 전화오면 받을까 말까 고민하다 받음.'
   },
   {
-    name: 'ISFP <말캉이>',
+    name: '7 ISFP <말캉이>',
     desc: '귀찮음, 행동 느림. 감정기복 심하고 공감능력 좋음. 모든 일 미룰 수 있을 때까지 다 미룸. 집순이, 집돌이. 집에가면 연락두절됨.'
   },
   {
-    name: 'ENTJ <라라>',
+    name: '8 ENTJ <라라>',
     desc: '팀플하면 결국 총대매고 내가 함. 누구한테 의존하거나 의지하는 스타일 아님. 오직 세상에 믿을 사람은 나 하나. 열등감 느낀적 극히 드뭄. 피해 주는거, 피해 받는거 싫어하고 남 일에 노관심.'
   },
   {
-    name: 'ENFJ <배불뚝이>',
+    name: '9 ENFJ <배불뚝이>',
     desc: '시끄러움. 핵인싸 되고싶어함. 사람들을 이끄는 것에 타고난 기질이 있고 좋아하기도 함. 사람을 되게 좋아함. 상대방도 본인을 신뢰한다는 느낌을 받으면 살아있음을 느낌. 단순함. 멘탈 강함.'
   },
   {
-    name: 'ESTJ <수호대장>',
+    name: '10 ESTJ <수호대장>',
     desc: '고집 셈, 현실적, 이성적, 직설적. 호불호 확실. 사람 많은거 싫어함. 리더 맡는거 싫어하는데 막상 하면 잘함. 나가서 노는것보단 이것저것 배우는게 좋음. 외로움 별로 안탐.'
   },
   {
-    name: 'ESTP <깃털기사>',
+    name: '11 ESTP <깃털기사>',
     desc: '외로움 잘 탐. 손재주 좋음. 리더쉽 있음. 표현을 많이 함. 어른들이 좋아함. 밖에서 사람 만나는거 좋아하지만 게을러서 나가기까지가 싫음.'
   },
   {
-    name: 'ENTP <키키>',
+    name: '12 ENTP <키키>',
     desc: '혼자서 돌아다니는게 제일 편함. 독립심 강함. 탐욕적, 자존심, 고집 셈, 냉철함. 겉으론 인싸 속모습은 아싸. 나한테 잘해주는 사람은 두 배로 잘해주는데 나한테 못해주면 걍 1도 없음. 밖에 놀러가면 진짜 잘 노는데 집 안에 있는것도 좋음.'
   },
   {
-    name: 'ENFP <꽥꽥이>',
+    name: '13 ENFP <꽥꽥이>',
     desc: '정신산만함, 생각 많음, 고집 셈. 일머리 있음. 흥미있고 관심있는건 열정적 그러나 관심 없는건 별로 알고 싶어하지도 않음. 매번 나서서 무얼 하진 않는데 아무도 안나서면 답답해서 나서는 스타일. 낯을 좀 가리는데 풀리면 금방 친해지고 말도 많아짐.'
   },
   {
-    name: 'ESFJ <말랑이>',
+    name: '14 ESFJ <말랑이>',
     desc: '생각보다 철저함. 혼자 계획 세우고 그 계획 틀어지는 거 싫어함. 술자리 좋아함. 남 눈치 많이 봄. 책 읽고 영화보는거 좋아함. 상담, 고민 들어주는 거 잘함.'
   },
   {
-    name: 'ESFP <요롱뇽>',
+    name: '15 ESFP <요롱뇽>',
     desc: '성격 매우 급함. 우주최강오지랖. 생각없이 잘삼. 아무리 걱정되는 일 있어도 좀만 지나면 무생각. 하고싶다고 생각한거 다 해야됨 근데 하기싫은건 끝까지 미룸. 고집 셈.'
   }
 ]
--- js/start.js
+++ js/start.js
@@ -2,8 +2,14 @@ const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 const result = document.querySelector("#result");
 const endPoint = 12;
+const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-function addAnswer(answerText, qIdx){
+function calResult(){
+    var resultIdx = select.indexOf(Math.max(...select));
+    return resultIdx;
+}
+
+function addAnswer(answerText, qIdx, idx){
     var a = document.querySelector('.aBox');
     var answer = document.createElement('button');
     answer.classList.add('answerList');
@@ -23,6 +29,11 @@ function addAnswer(answerText, qIdx){
             children[i].style.animation = "fadeOut 0.5s";
         }
         setTimeout(()=>{
+            var target = qnaList[qIdx].a[idx].type;
+            for(let i=0; i<target.length; i++){
+                select[target[i]]+=1;
+            }
+
             for(let i=0; i<children.length; i++){
                 children[i].style.display = 'none';
             }
@@ -32,11 +43,48 @@ function addAnswer(answerText, qIdx){
 
 }
 
+function setResult(){
+    let point = calResult();
+    console.log(select)
+    console.log(point)
+
+    const resultName = document.querySelector('#resultName');
+    resultName.innerHTML = infoList[point].name;
+    const resultImg = document.querySelector('#resultImg');
+    var img = document.createElement('img');
+    var imgURL = 'img/image-'+point+'.png';
+    img.src = imgURL;
+    img.alt = point;
+    img.classList.add('img-fluid');
+    resultImg.appendChild(img);
+    
+    const resultDesc = document.querySelector('#resultDesc');
+    resultDesc.innerHTML = infoList[point].desc;
+}
+
+function goResult(){
+    qna.style.WebkitAnimation = "fadeOut 1s";
+    qna.style.animation = "fadeOut 1s";
+    setTimeout(()=>{
+        result.style.WebkitAnimation = "fadeIn 1s";
+        result.style.animation = "fadeIn 1s";
+        setTimeout(()=>{
+            qna.style.display="none";
+            result.style.display="block";
+        }, 450)
+    }, 450)
+    setResult();
+}
+
 function goNext(qIdx){
+    if (qIdx===endPoint){
+        goResult();
+        return;
+    }
     var q = document.querySelector('.qBox');
     q.innerHTML = qnaList[qIdx].q;
     for(let i in qnaList[qIdx].a){
-        addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+        addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
     }
     var statusBar = document.querySelector('.statusBar');
     statusBar.style.width = (100/endPoint) * (qIdx+1) + '%';
